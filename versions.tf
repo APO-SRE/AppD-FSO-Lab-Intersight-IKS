6b8c2f4fff58abba61c2bf82611bf7f27c1c52de
Ed Barberis <ed.barberis@gmail.com>
2022-04-11 14:57:43 -0400

    Upgraded Terraform to 1.1.8.

--- versions.tf
+++ versions.tf
@@ -1,6 +1,6 @@
 # Terraform ----------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.1.7"
+  required_version = ">= 1.1.8"
 
   required_providers {
     intersight = {
