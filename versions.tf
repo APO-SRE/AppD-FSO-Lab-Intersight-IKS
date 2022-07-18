95856c117653115b1919e774b4375a1a44d8d1fb
Ed Barberis <ed.barberis@gmail.com>
2022-07-18 11:42:29 -0400

    Upgraded Terraform to 1.2.5.

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 # Terraform ----------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.2.2"
+  required_version = ">= 1.2.5"
 
   required_providers {
     intersight = {
       source  = "CiscoDevNet/intersight"
-      version = ">= 1.0.28"
+      version = ">= 1.0.30"
     }
   }
 }
