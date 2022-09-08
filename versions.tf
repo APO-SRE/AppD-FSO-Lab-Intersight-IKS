a9cdb8fcdfed1721d9aa8a76cb79825580e7df9a
Ed Barberis <ed.barberis@gmail.com>
2022-09-08 15:51:46 -0400

    Upgraded Terraform to 1.2.9 and Intersight to 1.0.32.

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 # Terraform ----------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.2.7"
+  required_version = ">= 1.2.9"
 
   required_providers {
     intersight = {
       source  = "CiscoDevNet/intersight"
-      version = ">= 1.0.31"
+      version = ">= 1.0.32"
     }
   }
 }
